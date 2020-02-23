fae3fd1dcd1d0b20653832cb469fbb200600e90c
Jake Haberle <jake.haberle@gmail.com>
2020-02-23 13:40:38 -0700

    intial html jumbotron added

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// creating a random number between 1 and 120 for the player to guess
+
+var cry1 = "";
+var cry2 = "";
+var cry3 = "";
+var cry4 = "";
+var randomNumber = "";
+var userScore = 0;
+
+// scoreboard variables
+var wins = 0;
+var losses = 0;
+
+function() game () {
+
+    
+        var randomNumber = Math.floor(Math.random());
+        console.log(randomNumber);
+        return rando;
+    
+
+}
+
+    
