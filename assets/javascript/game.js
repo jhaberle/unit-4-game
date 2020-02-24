49388bd966c2537c2205e01c8399d93d72bbfdd6
Jake Haberle <jake.haberle@gmail.com>
2020-02-23 20:52:47 -0700

    each button now creates random number correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,49 @@
 // creating a random number between 1 and 120 for the player to guess
 
-var cry1 = "";
-var cry2 = "";
-var cry3 = "";
-var cry4 = "";
+var crystalValue1 = "";
+var crystalValue2 = "";
+var crystalValue3 = "";
+var crystalValue4 = "";
 var randomNumber = "";
 var userScore = 0;
+var myVariable = "";
 
 // scoreboard variables
 var wins = 0;
 var losses = 0;
 
+
+
+// function to create random number for each crystal
 function getRandomNumber() {
 
     
-        var cry1 = 1 + Math.floor(Math.random() * 12);
-        console.log(randomNumber);
-        return cry1;
+        var randomNumber = 1 + Math.floor(Math.random() * 12);
+        return randomNumber;
     }   
 
     $('#cry1').on('click', function() {
-        var myVariable = getRandomNumber();
-        console.log(myVariable);
+        var crystalValue1 = getRandomNumber();
+        console.log(crystalValue1);
+        return crystalValue1;
     });
 
+    $('#cry2').on('click', function() {
+        var crystalValue2 = getRandomNumber();
+        console.log(crystalValue2);
+        return crystalValue2;
+    });
 
-        // var cry2 = 1 + Math.floor(Math.random() * 12);
-        // console.log(randomNumber);
-        // return rando;
+    $('#cry3').on('click', function() {
+        var crystalValue3 = getRandomNumber();
+        console.log(crystalValue3);
+        return crystalValue3;
+    });
 
-        // var cry3 = 1 + Math.floor(Math.random() * 12);
-        // console.log(randomNumber);
-        // return rando;
-
-        // var cry4 = 1 + Math.floor(Math.random() * 12);
-        // console.log(randomNumber);
-        // return rando;
-
-
-
-
-
+    $('#cry4').on('click', function() {
+        var crystalValue4 = getRandomNumber();
+        console.log(crystalValue4);
+        return crystalValue4;
+    });
     
+    $('#current-score').
